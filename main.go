2edb973c51cb472dc5dc9345954e63f0f27de301
devpresss xmont <xmontemorjerald@gmail.com>
2025-12-10 13:26:24 +0800

    Modify slices

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-//This is comment
+// This is comment
 func main() {
-	//Create a slice from an array
-	myarray := [6]int{1, 2, 3, 4, 5, 6}
-	myslice := myarray[0:3]
-	fmt.Println(myslice)
-	fmt.Println(cap(myslice))
 
-	//Using make function
-	fmt.Println("********Using make()**********")
-	myslice1 := make([]int, 4, 6)
-	fmt.Println(len(myslice1))
-	fmt.Println(cap(myslice1))
+	//Access the values using index of the slice
+	prices := []float32{100, 400.00, 30.00}
+	prices[1] = 20.00
+	fmt.Println((prices[1]))
+
+	//Append function
+	fmt.Println("********Using append function to insert new value********")
+	prices = append(prices, 500, 900)
+	fmt.Println(prices)
+
+	//Append one slice to another one
+	fmt.Println("********Using append function to append another slice********")
+	my_slice_1 := []int{1, 2, 3, 4, 5}
+	my_slice_2 := []int{6, 7, 8, 9, 10}
+	my_slice_1 = append(my_slice_1, my_slice_2...)
+	fmt.Println(my_slice_1)
 
 }
