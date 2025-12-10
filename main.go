926c33ca0d1ce762634985eae32bed2170a3aea9
devpresss xmont <xmontemorjerald@gmail.com>
2025-12-10 13:30:40 +0800

    Addiditon

--- main.go
+++ main.go
@@ -7,21 +7,12 @@ import (
 // This is comment
 func main() {
 
-	//Access the values using index of the slice
-	prices := []float32{100, 400.00, 30.00}
-	prices[1] = 20.00
-	fmt.Println((prices[1]))
+	var a = 15 + 15
+	fmt.Println(a)
 
-	//Append function
-	fmt.Println("********Using append function to insert new value********")
-	prices = append(prices, 500, 900)
-	fmt.Println(prices)
+	var b = 9
+	var c = 11
 
-	//Append one slice to another one
-	fmt.Println("********Using append function to append another slice********")
-	my_slice_1 := []int{1, 2, 3, 4, 5}
-	my_slice_2 := []int{6, 7, 8, 9, 10}
-	my_slice_1 = append(my_slice_1, my_slice_2...)
-	fmt.Println(my_slice_1)
-
+	var result = b + c
+	fmt.Print(result)
 }
