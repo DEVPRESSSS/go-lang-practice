5de300eb723437059d6b8b166704289e4401d929
devpresss xmont <xmontemorjerald@gmail.com>
2025-12-10 10:39:38 +0800

    Print functions

--- main.go
+++ main.go
@@ -5,48 +5,12 @@ import "fmt"
 //This is comment
 func main() {
 
-	//With keyword datatype
-	fmt.Println("***Multiple variable with initial value***")
-	var a, b, c int = 1, 2, 3
-	fmt.Println(a, b, c)
-
-	//With string and bool
-	fmt.Println("**************With string and bool*****************")
-
-	var age, name, gay = 22, "Jerald", false
-	fmt.Println(age, name, gay)
-
-	fmt.Println("******************************************")
-
-	r, d := 34, "Panpan"
-	fmt.Println(r, d)
-
-	//Variable declartion in a block
-
 	var (
-		cat1    string = "Panpan"
-		cat1Age        = 3
-		sound   string = "Meow"
+		name     = "Jerald"
+		lastname = "Montemor"
+		age      = 22
 	)
-	fmt.Println(cat1, cat1Age, sound)
-
-	fmt.Println("*****************PI Value************************")
-
-	//This is the typed constant
-	const piValue float32 = 3.14
-
-	//Untyped constants
-	const pivalue2 = 3.14
-
-	fmt.Println(piValue)
-	fmt.Println(pivalue2)
-
-	//Using declaration of constant in a block
-	const (
-		username = "Jerald"
-		ign      = "BSCSJIRO"
-		program  = "BSCS"
-	)
-
-	fmt.Println(username, ign, program)
+	//Can print the values using %v
+	fmt.Printf("This is the value of the name %v: \n This is the value of lastname: %v : \n this is the value of the age: %v", name, lastname, age)
+	fmt.Printf("Types: %T,%T,%T", name, lastname, age)
 }
